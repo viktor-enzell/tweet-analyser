ab19684b09bcf16ea653d6de6ec12af505cf766d
viktor-enzell <viktor.enzell@gmail.com>
2021-10-04 16:17:30 +0200

    Changed sentiment model.

--- django_project/sentiment_model.py
+++ django_project/sentiment_model.py
@@ -1,12 +1,13 @@
-import pickle 
+import pickle
 
-# Predicts avrage sentiment based on list of texts
+
+# Predicts average sentiment based on list of texts
 class SentimentModel:
 
     # Load both the support vector classifer aswell as the tfid vectorizer
     def __init__(self):
-        self.model = pickle.load(open('model.sav', 'rb'))
-        self.vec = pickle.load(open('vectorizer.pk', 'wb'))
+        self.model = pickle.load(open('model/model.sav', 'rb'))
+        self.vec = pickle.load(open('model/vectorizer.pk', 'rb'))
 
     # Takes a list of texts and returns sentiment from 0 to 1
     def get_sentiment(self, data):
