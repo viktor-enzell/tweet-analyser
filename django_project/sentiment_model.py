035e43a0a622fd6f3a95bb3270757839ee9f4d2f
adam <adam.v.hoewijk@gmail.com>
2021-10-04 16:09:13 +0200

    Updated saving of model

--- django_project/sentiment_model.py
+++ django_project/sentiment_model.py
@@ -1,12 +1,14 @@
 import pickle 
 
+directory = 'django_project/models/'
+
 # Predicts avrage sentiment based on list of texts
 class SentimentModel:
 
     # Load both the support vector classifer aswell as the tfid vectorizer
     def __init__(self):
-        self.model = pickle.load(open('model.sav', 'rb'))
-        self.vec = pickle.load(open('vectorizer.pk', 'wb'))
+        self.model = pickle.load(open(directory+'model.sav', 'rb'))
+        self.vec = pickle.load(open(directory+'vectorizer.pk', 'rb'))
 
     # Takes a list of texts and returns sentiment from 0 to 1
     def get_sentiment(self, data):
